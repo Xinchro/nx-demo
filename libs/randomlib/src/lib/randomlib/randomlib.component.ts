c7429e2e06c3bb9709410f3dfac76686b33dad14
Walter Reis <9714275+Xinchro@users.noreply.github.com>
2024-07-24 15:11:38 +0100

    added randomapp and randomlib, and moved over relevant nx config

---
+++ libs/randomlib/src/lib/randomlib/randomlib.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-randomlib',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './randomlib.component.html',
+  styleUrl: './randomlib.component.css',
+})
+export class RandomlibComponent {}
